9202b39287563306b22c60726ffc006af81f3783
xVanAnDayNe <hoangvanan5978@gmail.com>
2024-10-17 20:07:08 +0700

    Cors

--- SWP_Ticket_ReSell_API/Program.cs
+++ SWP_Ticket_ReSell_API/Program.cs
@@ -64,10 +64,8 @@ builder.Services.AddCors(options =>
               .AllowAnyMethod();
     });
 });
-
 // Add Firebase Storage service
 builder.Services.AddScoped<FirebaseStorageService>();
-
 // Add DBContext for SQL Server
 builder.Services.AddDbContext<swp1Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("MyDB"))
