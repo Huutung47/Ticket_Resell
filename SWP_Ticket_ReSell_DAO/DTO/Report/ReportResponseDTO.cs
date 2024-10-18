c276606f432b209b6d5e64134d80071bc48e5087
xVanAnDayNe <hoangvanan5978@gmail.com>
2024-10-18 21:35:57 +0700

    Update report

--- SWP_Ticket_ReSell_DAO/DTO/Report/ReportResponseDTO.cs
+++ SWP_Ticket_ReSell_DAO/DTO/Report/ReportResponseDTO.cs
@@ -12,7 +12,7 @@ namespace SWP_Ticket_ReSell_DAO.DTO.Report
 
         public int ID_Customer { get; set; }
 
-        public int ID_Order { get; set; }
+        //public int ID_Order { get; set; }
 
         public string Comment { get; set; }
 
