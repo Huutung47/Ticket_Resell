6f5eb930222fd1f04875aa1cb9c2888caf92a444
xVanAnDayNe <hoangvanan5978@gmail.com>
2024-11-11 17:14:55 +0700

    Fix database

--- SWP_Ticket_ReSell_DAO/Models/Order.cs
+++ SWP_Ticket_ReSell_DAO/Models/Order.cs
@@ -17,7 +17,7 @@ public partial class Order
 
     public DateTime? Shipping_time { get; set; }
 
-    public DateTime? Create_At { get; set; }
+    public DateTime Create_At { get; set; }
 
     public decimal? TotalPrice { get; set; }
 
